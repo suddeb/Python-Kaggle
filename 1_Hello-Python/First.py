90a6d479f4c4424c78d20d359cb58ae0195944eb
Sudipta Deb <sudipta.deb@gmail.com>
2024-10-25 12:36:53 -0400

    Multiple lines under single if statement

--- 1_Hello-Python/First.py
+++ 1_Hello-Python/First.py
@@ -6,6 +6,8 @@ spam_amount = spam_amount + 4
 
 if spam_amount > 0:
     print("But I don't want ANY spam!")
+    print("I am still inside if statement")
+print("Now I am outside if statement");
 
 viking_song = "Spam " * spam_amount
 print(viking_song)
