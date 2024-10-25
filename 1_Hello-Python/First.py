8a2ffd4c4d70f0a575b41beb01cc9d44d62d7dd9
Sudipta Deb <sudipta.deb@gmail.com>
2024-10-25 12:40:34 -0400

    Added type() function

--- 1_Hello-Python/First.py
+++ 1_Hello-Python/First.py
@@ -1,6 +1,8 @@
 spam_amount = 0
 print(spam_amount)
 
+print(type(spam_amount))
+
 # Ordering Spam, egg, Spam, Spam, bacon and Spam (4 more servings of Spam)
 spam_amount = spam_amount + 4
 
@@ -11,3 +13,4 @@ print("Now I am outside if statement");
 
 viking_song = "Spam " * spam_amount
 print(viking_song)
+print(type(viking_song))
