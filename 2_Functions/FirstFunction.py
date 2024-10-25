9bd0f94f639bf7dcd5b6f41ff1f008979c54b6bf
Sudipta Deb <sudipta.deb@gmail.com>
2024-10-25 14:21:12 -0400

    Function with Parameters

--- 2_Functions/FirstFunction.py
+++ 2_Functions/FirstFunction.py
@@ -1,4 +1,10 @@
 def leastDifferences (a, b, c):
+    """Return the smallest difference between any two numbers
+    among a, b and c.
+    
+    >>> least_difference(1, 5, -5)
+    4
+    """
     diff1 = abs(a-b)
     diff2 = abs(b-c)
     diff3 = abs(a-c)
@@ -9,3 +15,15 @@ print(
     leastDifferences(1, 10, 10),
     leastDifferences(5, 6, 7),
 )
+
+help(leastDifferences)
+
+print(1,2,3,sep=' < ')
+
+def greeting(who="Deb"):
+    print("hello, ",who)
+    
+
+greeting()
+greeting(who="Sudipta")
+greeting("James")
