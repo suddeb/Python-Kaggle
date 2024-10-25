0187c940adb404629ad1d13949c450d835906ac3
Sudipta Deb <sudipta.deb@gmail.com>
2024-10-25 15:27:40 -0400

    Boolean Function

---
+++ 3_BooleanConditionals/BooleanConditionals.py
@@ -0,0 +1,10 @@
+def can_drive(age):
+    """can_drive?
+    # As per the rule, you need to be atleast 18 years old to drive car
+    Args:
+        age (int): age of the person
+    """
+    return age >=18
+
+print(can_drive(12))
+print(can_drive(23))
